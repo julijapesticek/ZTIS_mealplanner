064a32001324bfafd9baa8040fa5bce854d42dab
Julija <julija.pesticek@student.um.si>
2023-05-10 20:38:40 +0200

    Hash password

--- models/users.js
+++ models/users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     // id: {
@@ -36,5 +37,17 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 });
+
+userSchema.pre('save', async function(next) {
+    const user = this;
+    if (!user.isModified('password')) {
+      return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+    user.password = hashedPassword;
+    next();
+  });
+
 const User = mongoose.model('user', userSchema, 'Users');
 module.exports = User;
